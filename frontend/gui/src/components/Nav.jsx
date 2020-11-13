fa644fb254bc06f1c568a1cb6117e56c88612b39
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-11-13 22:16:49 +0100

    Set up react

---
+++ frontend/gui/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <nav>
+          <div className="container"></div>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Nav;
---
+++ frontend/gui/src/containers/Layout.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+       <div></div>
+      </>
+     );
+  }
+}
+ 
+export default Layout;
